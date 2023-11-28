d713e44435ceee33be7349859749197035c04e4d
Munsa1 <munsamibenge65@gmail.com>
2023-11-28 11:57:35 +0200

    Add navigation bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './header.scss';
+import myLogo from '../../assests/graphics/logo.png'
 const Header = ()=>{
 
     return(
         <header>
             <div className="wrap">
                 <div className="logo">
+                    <img src={myLogo} alt="Logo" />
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Contact</li>
+                        <li>About</li>
+                    </ul>
 
                 </div>
             </div>
